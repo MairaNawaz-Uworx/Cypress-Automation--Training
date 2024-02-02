dcbd109409bca20a63c6b576245d101a44436b2b
MairaNawaz-Uworx <maira.nawaz@uworx.co.uk>
2024-02-02 13:54:30 +0500

    Third Commit

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,10 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: 'pikuj7',
+  reporter: 'cypress-multi-reporters',
+  reporterOptions: {
+    configFile: 'reporter-config.json',
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
